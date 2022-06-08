dce752f1f127763a4d0a8d08a224984dd4353a6d
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-06-08 12:29:13 +0200

    should create a new route

--- test/transport-price-checker.tests.js
+++ test/transport-price-checker.tests.js
@@ -1,6 +1,21 @@
 describe("Testing for my TransportPriceChecker factory function", function() {
-    // the new route created will be an object
     it("should be able to create a new route", function () {
-        assert.deepEqual({},{})
+        const transportPriceChecker = TransportPriceChecker()
+
+        // the new route created will be an object
+        const expexted = {
+            route: "KHAYELITSHA-CAPE TOWN",
+            taxiFare: 20,
+            busFare: 21
+        }
+
+        // to set a new route you need to the route the taxi fare and the bus fare
+        transportPriceChecker.setNewRoute("KHAYELITSHA-CAPE TOWN", 20, 21)
+
+        // get the route that was created
+        const results = transportPriceChecker.getNewRoute()
+
+        // test to check if the created route is the same as the one expected
+        assert.deepEqual(results, expexted)
     })
 })
---
+++ transport-price-checker.js
@@ -0,0 +1,3 @@
+function TransportPriceChecker (){
+    return
+}
