58f403a444afaf38658ff0a76245900a4514f7f3
ZezethuM <zezethu15@gmail.com>
2022-06-09 00:08:24 +0200

    dom-link

--- DOM-price-checker.js
+++ DOM-price-checker.js
@@ -1,7 +1,15 @@
+//For route.html
 const availableRoutes = document.querySelector("#route");
 const busPrice = document.querySelector(".busPrice");
 const taxiPrice = document.querySelector(".taxiPrice");
 const numberOfTrips = document.querySelector(".tripNumber");
+
+//For addRoute.html
+const departLocation = document.querySelector('.depart');
+const arrivalLocation = document.querySelector('.arrive');
+const taxiPrice2 = document.querySelector('.taxiPrice');
+const busPrice2 = document.querySelector('.busPrice');
+const addRoutBtn = document.querySelector('.addRoutBtn');
 
 const pricePlanner = TransportPriceChecker();
 
@@ -10,4 +18,9 @@ availableRoutes.addEventListener('change', function(){
     const route = pricePlanner.getRouteInfo(availableRoutes.value);
     taxiPrice.value = route.taxiFare;
     busPrice.value = route.busFare;
-})
+});
+
+addRoutBtn.addEventListener('click', () =>{
+    
+});
+
